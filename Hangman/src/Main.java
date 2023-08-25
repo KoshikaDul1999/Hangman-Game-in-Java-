a045f51d73e7586ef9d2649958a302a05d7c54ce
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-08-25 09:23:51 +0530

    Add words to an array

--- Hangman/src/Main.java
+++ Hangman/src/Main.java
@@ -1,4 +1,18 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        Scanner scanner = new Scanner(new File("D:/Lectures/Programming/Java/Hangman-Game-in-Java-/words.txt/"));
+
+        List<String> words = new ArrayList<>();
+        while (scanner.hasNext()) {
+            words.add(scanner.nextLine());
+        }
+
     }
 }
