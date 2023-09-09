ea21b2306c2a18493ea593a5a8462894c82b0194
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-09-09 18:22:42 +0530

    Update Main.java

--- Hangman/src/Main.java
+++ Hangman/src/Main.java
@@ -9,13 +9,16 @@ public class Main {
     public static void main(String[] args) throws FileNotFoundException {
 
         Scanner keyboard = new Scanner(System.in);
+
         System.out.println("1 or 2 players?");
         String players = keyboard.nextLine();
-        String word;
-        if (players.equals("1")){
-            Scanner scanner = new Scanner(new File("D:/Lectures/Programming/Java/Hangman-Game-in-Java-/words.txt/"));
 
+        String word;
+
+        if (players.equals("1")) {
+            Scanner scanner = new Scanner(new File("C:/Users/johnm/hangman/words_alpha.txt"));
             List<String> words = new ArrayList<>();
+
             while (scanner.hasNext()) {
                 words.add(scanner.nextLine());
             }
@@ -24,34 +27,39 @@ public class Main {
             word = words.get(rand.nextInt(words.size()));
         }
         else {
-            System.out.println("Player 1, please enter your word");
+            System.out.println("Player 1, please enter your word:");
             word = keyboard.nextLine();
+            System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+            System.out.println("Ready for player 2! Good luck!");
         }
 
-        System.out.println(word);
+        //System.out.println(word);
 
         List<Character> playerGuesses = new ArrayList<>();
 
-        int wrongCount = 0;
-        while (true) {
+        Integer wrongCount = 0;
+
+        while(true) {
             printHangedMan(wrongCount);
 
-            if (wrongCount >= 6){
+            if (wrongCount >= 6) {
                 System.out.println("You lose!");
+                System.out.println("The word was: " + word);
                 break;
             }
 
             printWordState(word, playerGuesses);
-            if (!getPlayerGuess(keyboard, word, playerGuesses)){
+            if (!getPlayerGuess(keyboard, word, playerGuesses)) {
                 wrongCount++;
             }
 
-            if (printWordState(word, playerGuesses)){
+            if(printWordState(word, playerGuesses)) {
                 System.out.println("You win!");
                 break;
             }
+
             System.out.println("Please enter your guess for the word:");
-            if (keyboard.nextLine().equals(word)){
+            if(keyboard.nextLine().equals(word)) {
                 System.out.println("You win!");
                 break;
             }
